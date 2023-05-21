c39cefc5be4c6c7a34565fd6ea084aa82d9c8c14
cameron-green-1 <87066027+cameron-green-1@users.noreply.github.com>
2023-05-21 15:28:10 +0100

    get rough draft version of follow mouse thing working

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import { gsap } from 'gsap';
@@ -10,15 +10,85 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 
 export default function Home() {
     useEffect(() => {
-        // create the smooth scroller FIRST!
+        let scrollY = 0;
         const smoother = ScrollSmoother.create({
-            wrapper: '.wrapper',
-            content: '.content',
+            wrapper: '.smooth-wrapper',
+            content: '.smooth-content',
             smooth: 2,
             normalizeScroll: true, // prevents address bar from showing/hiding on most devices, solves various other browser inconsistencies
             ignoreMobileResize: true, // skips ScrollTrigger.refresh() on mobile resizes from address bar showing/hiding
             effects: true,
             preventDefault: true,
+            onUpdate: (self) => {
+                scrollY = self.scrollTop();
+            },
+        });
+
+        const target = document.getElementById('move');
+        const about = document.getElementById('about');
+        // const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+        const pos = { x: 0, y: 0 };
+        const mouse = { x: pos.x, y: pos.y };
+        const speed = 0.05;
+        const xSet = gsap.quickSetter(target, 'x', 'px');
+        const ySet = gsap.quickSetter(target, 'y', 'px');
+        let movementAllowed = false;
+
+        window.addEventListener('mousemove', (e) => {
+            mouse.x = e.x;
+            mouse.y = e.y;
+        });
+
+        gsap.ticker.add(() => {
+            // adjust speed for higher refresh monitors
+            const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+
+            if (movementAllowed) {
+                pos.x += (mouse.x - (target.offsetLeft + 250) - pos.x) * dt;
+                // pos.y +=
+                //     (mouse.y - (target.offsetTop + scrollY - 250) - pos.y) * dt;
+                pos.y +=
+                    (mouse.y + scrollY - (target.offsetTop + 250) - pos.y) * dt;
+                xSet(pos.x);
+                ySet(pos.y);
+            } else {
+                pos.x += (0 - pos.x) * dt;
+                pos.y += (0 - pos.y) * dt;
+                xSet(pos.x);
+                ySet(pos.y);
+            }
+        });
+        gsap.to(target, {
+            // duration: 1,
+            backgroundColor: 'rgb(220, 220, 220)',
+            // scale: 0.5,
+            width: 500,
+            height: 500,
+            borderRadius: 500,
+            scrollTrigger: {
+                trigger: intro,
+                toggleActions: 'restart none none reverse',
+                // scrub: true,
+                start: 'top top',
+                // end: about,
+                // end: 'top 30%',
+                markers: true,
+                onEnter: () => {
+                    console.log('onEnter');
+                    movementAllowed = true;
+                },
+                onEnterBack: () => {
+                    console.log('onEnterBack');
+                    movementAllowed = true;
+                },
+                onLeave: () => {
+                    movementAllowed = false;
+                },
+                onLeaveBack: () => {
+                    console.log('onLeaveBack');
+                    movementAllowed = false;
+                },
+            },
         });
     }, []);
     return (
@@ -35,90 +105,111 @@ export default function Home() {
                 />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
-            <div className='wrapper'>
-                <main className='content'>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <section className={styles.vCenter}>
+            <div className='smooth-wrapper'>
+                <div className='smooth-content'>
+                    <div className={styles.wrapper}>
+                        <main className={styles.main}>
+                            <section className={styles.intro} id='intro'>
+                                <h1>
+                                    SMART
+                                    <br />
+                                    DEVELOPMENT_
+                                </h1>
+                                <div className={styles.move} id='move'></div>
+                                <div className={styles.about} id='about'>
+                                    <h2>ABOUT</h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipiscing elit. Donec tincidunt
+                                        suscipit neque, ac pretium est. In non
+                                        elementum nisi. Proin iaculis arcu est,
+                                        sit amet fringilla sapien auctor at.
+                                        Morbi fermentum ut mauris sed porttitor.
+                                        Mauris mollis viverra risus quis
+                                        pulvinar. Aenean pretium ligula sed eros
+                                        egestas volutpat. Nunc in porta est.
+                                        Aliquam ut volutpat justo. Integer quam
+                                        tortor, lacinia eget lacus non, semper
+                                        imperdiet dolor. Sed lacinia dolor quis
+                                        sem suscipit venenatis. Proin fermentum
+                                        elit turpis, sit amet blandit diam
+                                        tincidunt sollicitudin. Praesent luctus
+                                        orci metus, sed vehicula mi maximus
+                                        finibus. Pellentesque volutpat rhoncus
+                                        tempor.
+                                    </p>
+                                </div>
+                            </section>
+                            <div className={styles.code}>
+                                <h2>CODE</h2>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Donec tincidunt suscipit
+                                    neque, ac pretium est. In non elementum
+                                    nisi. Proin iaculis arcu est, sit amet
+                                    fringilla sapien auctor at. Morbi fermentum
+                                    ut mauris sed porttitor. Mauris mollis
+                                    viverra risus quis pulvinar. Aenean pretium
+                                    ligula sed eros egestas volutpat. Nunc in
+                                    porta est. Aliquam ut volutpat justo.
+                                    Integer quam tortor, lacinia eget lacus non,
+                                    semper imperdiet dolor. Sed lacinia dolor
+                                    quis sem suscipit venenatis. Proin fermentum
+                                    elit turpis, sit amet blandit diam tincidunt
+                                    sollicitudin. Praesent luctus orci metus,
+                                    sed vehicula mi maximus finibus.
+                                    Pellentesque volutpat rhoncus tempor.
+                                </p>
+                                <p>
+                                    Praesent fringilla, neque et ornare
+                                    efficitur, sapien ex accumsan erat, eget
+                                    placerat nisi elit sit amet lacus. Aliquam
+                                    erat volutpat. Sed facilisis consectetur
+                                    egestas. Sed varius pellentesque vulputate.
+                                    Cras vitae lacinia nibh. Quisque eget semper
+                                    dui. Ut congue sem non mollis vestibulum.
+                                    Morbi vestibulum nisi orci, sit amet
+                                    tincidunt leo eleifend ut. Cras vulputate
+                                    faucibus vulputate. Donec consectetur, nulla
+                                    vitae vestibulum porta, nulla nulla dictum
+                                    odio, in aliquet lorem elit non dolor.
+                                    Curabitur tincidunt varius sollicitudin.
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur
+                                    adipiscing elit. Donec tincidunt suscipit
+                                    neque, ac pretium est. In non elementum
+                                    nisi. Proin iaculis arcu est, sit amet
+                                    fringilla sapien auctor at. Morbi fermentum
+                                    ut mauris sed porttitor. Mauris mollis
+                                    viverra risus quis pulvinar. Aenean pretium
+                                    ligula sed eros egestas volutpat. Nunc in
+                                    porta est. Aliquam ut volutpat justo.
+                                    Integer quam tortor, lacinia eget lacus non,
+                                    semper imperdiet dolor. Sed lacinia dolor
+                                    quis sem suscipit venenatis. Proin fermentum
+                                    elit turpis, sit amet blandit diam tincidunt
+                                    sollicitudin. Praesent luctus orci metus,
+                                    sed vehicula mi maximus finibus.
+                                    Pellentesque volutpat rhoncus tempor.
+                                </p>
+                                <p>
+                                    Praesent fringilla, neque et ornare
+                                    efficitur, sapien ex accumsan erat, eget
+                                    placerat nisi elit sit amet lacus. Aliquam
+                                    erat volutpat. Sed facilisis consectetur
+                                    egestas. Sed varius pellentesque vulputate.
+                                    Cras vitae lacinia nibh. Quisque eget semper
+                                    dui. Ut congue sem non mollis vestibulum.
+                                    Morbi vestibulum nisi orci, sit amet
+                                    tincidunt leo eleifend ut. Cras vulputate
+                                    faucibus vulputate. Donec consectetur, nulla
+                                    vitae vestibulum porta, nulla nulla dictum
+                                    odio, in aliquet lorem elit non dolor.
+                                    Curabitur tincidunt varius sollicitudin.
+                                </p>
+                            </div>
+                            {/* <section className={styles.vCenter}>
                         <div className={styles.parallaxSlab}>
                             <img
                                 data-speed='auto'
@@ -126,53 +217,10 @@ export default function Home() {
                                 alt=''
                             />
                         </div>
-                    </section>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                    <h1>
-                        ART
-                        <br />
-                        DIRECTION
-                    </h1>
-                </main>
+                    </section> */}
+                        </main>
+                    </div>
+                </div>
             </div>
         </>
     );
